4e19220353de867bb7b2f8e126d555e53cdf4a66
Amit Ranjan <amitalable@gmail.com>
2021-11-12 22:20:24 +0530

    Day-12

--- Week-2/RemoveLinkedListElemnts.py
+++ Week-2/RemoveLinkedListElemnts.py
@@ -4,6 +4,13 @@
 #         self.val = val
 #         self.next = next
 # https://leetcode.com/problems/remove-linked-list-elements
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         if not head:
