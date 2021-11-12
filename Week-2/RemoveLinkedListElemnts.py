2bcc918da8314eb5b15c964c544a3e702ac24da1
Amit Ranjan <amitalable@gmail.com>
2021-11-12 22:19:33 +0530

    Day-12

---
+++ Week-2/RemoveLinkedListElemnts.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# https://leetcode.com/problems/remove-linked-list-elements
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        temp = head
+        prev = None
+        while temp:
+            if temp.val == val:
+                if prev:
+                    prev.next = temp.next
+                else:
+                    head = temp.next
+            else:
+                prev = temp
+            temp = temp.next    
+        return head
